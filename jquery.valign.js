d3e8324a50d5752be47a02091ae5eedda82c8618
Sean Jones <seany.jones@googlemail.com>
2013-04-24 16:45:20 +0200

    added a default option

--- jquery.valign.js
+++ jquery.valign.js
@@ -4,7 +4,7 @@
         Elements should probably be hidden via CSS until this function is complete.
         e.g 
         .element {
-          visibility:hidden;        
+	        visibility:hidden;        
         }
         .valign {
 	        visibility:visible;
@@ -12,17 +12,23 @@
     */
 
     $.fn.valign = function (options) {
+        var settings = $.extend({}, $.fn.valign.defaultOptions, options);
 
         return this.each(function () {
             var elem = $(this),
                 container = elem.parent(),
                 elemHeight = elem.outerHeight(),
                 containerHeight = container.height(),
-                topMargin = (containerHeight - elemHeight) / 2;
+                topMargin = (containerHeight - elemHeight) / 2,
+                spacingTop = settings.style;
 
-            elem.css({marginTop: Math.floor(topMargin)}).addClass('valign');
+            elem.css( spacingTop, Math.floor(topMargin) ).addClass('valign');
         });
 
+    };
+
+    $.fn.valign.defaultOptions = {
+        style: 'padding-top'
     };
 
 })(jQuery);
